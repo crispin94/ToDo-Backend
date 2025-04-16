7b0b787db12b1ef70280d5fc3b5d675170df19b1
Abraham Crispin <abraham.crispin@proveedoresgnp.mx>
2025-04-16 16:06:30 -0600

    restauracion de archivos y limpieza de files innecesarios

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAppBack.Models;
+
+namespace ToDoAppBack.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
